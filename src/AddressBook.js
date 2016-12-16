97f7013ab516692d62c5cd86f908c2943615f667
maindev <systemdevcloud@live.co.uk>
2016-12-16 16:10:48 +0000

    firstupload addressbook

---
+++ src/AddressBook.js
@@ -0,0 +1,25 @@
+
+function AddressBook() {
+
+// add an array of items
+
+var contacts = [];
+
+
+
+}
+
+
+AddressBook.prototype.addContact = function(contact) {
+
+	this.contacts.push(contact);
+
+}
+
+/*
+AddressBook.prototype.getContact = function(index) {
+
+	return this.contacts[index];
+}
+*/
+
